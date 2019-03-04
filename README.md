d77aec3ef6d4beb7be52bc1457f6b5277ac59cb8
mktosos <taniguchidesigns@hotmail.com>
2019-03-03 21:23:43 -0800

    README.md added text

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # liri-node-app
+This project allows a user to search for movie info using the OMDB API, concerts by artists with Bands in Town API, and songs from Spotify API. The search parameters are inputed in the terminal in the format: node liri.js <parameter1 - action> <parameter2 - name>. Action commands are:
+* `concert-this`
+* `spotify-this-song`
+* `movie-this`
+* `do-what-it-says`
+example request: node liri.js movie-this harry potter
+
+Known issues: 
+Spotify search not working properly.
+
+The project uses API access to get data sets which are parsed for specific key values to display via console.log in the terminal. This application is run completely in the terminal uning node.js and required modules, some installed using NPM install.
+
+
+Credits: The game project student developer is Mike Taniguchi.
+
+License The MIT License (MIT)
+
+Copyright (c) 2019 Mike Taniguchi
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
--- liri.js
+++ liri.js
@@ -5,7 +5,7 @@ var axios = require("axios");
 var dateFormat = require('dateformat');
 var now = new Date();
 var fs = require("fs");
-var firstTime = true;
+// var firstTime = true;
 mainFunction();
  
 function mainFunction(action,search){
@@ -48,7 +48,7 @@ function mainFunction(action,search){
         console.log('Title:', response.data.Title); 
         console.log('Year:', response.data.Year); 
         console.log('IMDB Rating:', response.data.imdbRating);
-        console.log('Metascore:', response.data.Metascore);
+        console.log('Rotten Tomatoes:', response.data.Ratings[1].Value);
         console.log('Produced in',response.data.Country);
         console.log('Language:',response.data.Language);
         console.log('Plot:', response.data.Plot);
@@ -67,18 +67,18 @@ function mainFunction(action,search){
         console.log(error.config);
     });
     } 
-    else if (firstTime === true && process.argv[2]===`do-what-it-says`){
+    else if (process.argv[2]===`do-what-it-says`){
         fs.readFile('random.txt', 'utf-8', function(error, data){
             //console.log(data.split(",")[0]);
             var res = data.split(",")[1].replace('"', '');
             var replacedRes = res.replace('"', '');
-            console.log(replacedRes);
+            //console.log(replacedRes);
             // firstTime = false;
             // mainFunction(data.split(",")[0],replacedRes);
-            console.log(process.argv[2]);
+            //console.log(process.argv[2]);
             process.argv[2]=data.split(",")[0];
             process.argv[3]=replacedRes;
-            console.log(process.argv[2]);
+            //console.log(process.argv[2]);
 
             if (process.argv[2]==='concert-this'){
                 axios
@@ -113,7 +113,7 @@ function mainFunction(action,search){
                         axios
                 .get("http://www.omdbapi.com/?apikey=trilogy&t=" + process.argv[3])
                 .then(function(response) {
-                    console.log(response.data)
+                    //console.log(response.data)
                     console.log('Title:', response.data.Title); 
                     console.log('Year:', response.data.Year); 
                     console.log('IMDB Rating:', response.data.imdbRating);
@@ -139,19 +139,36 @@ function mainFunction(action,search){
 
         });
     } 
-    
-}    
+    // else if (process.argv[2]===`spotify-this-song`){
+    //     function spotif(){
+    //         // var songId = process.argv.slice(3).join(” “);
+    //         var Spotify = require('node-spotify-api');
+ 
+    //         var spotify = new Spotify({
+    //           id: <your spotify client id>,
+    //           secret: <your spotify client secret>
+    //         });
+             
+    //         spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
+    //           if (err) {
+    //             return console.log('Error occurred: ' + err);
+    //           }
+             
+        //     console.log(data); 
+        //     });
+        //       var songs = data.tracks.items;
+        //       for (var i =0; i < songs.length; i++) {
+        //         console.log(“Song name: “, songs[i].name);
+        //         // console.log(“Artist: “, songs[i].album[1]);
+        //         console.log(“Album: “, songs[i].album.name);
+        //         console.log(“Preview song: “, songs[i].preview_url);
+        //         console.log(“------------------------------------“);
+        //       }
+        //     });
+        //    };   
+    // }
+}
+
 
 // * `spotify-this-song`***needs work/ not working
-
-// `movie-this`******** working DONE
-    //    * Title of the movie.
-    //    * Year the movie came out.
-    //    * IMDB Rating of the movie.
-    //    * Rotten Tomatoes Rating of the movie.
-    //    * Country where the movie was produced.
-    //    * Language of the movie.
-    //    * Plot of the movie.
-    //    * Actors in the movie.
-    //  ```
 
